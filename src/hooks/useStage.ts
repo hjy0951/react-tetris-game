6b5e89195defe9d2432024a4b3e9816b1806ce64
hjy0951 <hjy0951@gmail.com>
2023-04-22 17:00:22 +0900

    feat: useStage에 호출 시그니처 추가

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 // util
 import { createStage } from "../util/gameHelper";
 import { StageFormat } from "../components/Stage";
@@ -7,7 +7,10 @@ import { StageFormat } from "../components/Stage";
 현재 테트리스 게임판의 상태를 저장하기 위한 hook
 */
 
-export const useStage = () => {
+export const useStage = (): [
+  StageFormat,
+  Dispatch<SetStateAction<StageFormat>>
+] => {
   const [stage, setStage] = useState<StageFormat>(createStage());
 
   return [stage, setStage];
