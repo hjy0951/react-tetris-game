2e493aef824cbe6893707485d3b6bd5e2c30d6a3
hjy0951 <hjy0951@gmail.com>
2023-04-22 18:18:03 +0900

    feat: Player 상태의 변경에 따라 Stage가 update 되고 이에 따른 렌더링이 이루어지도록 구현

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -1,21 +1,58 @@
-import React, { useState } from "react";
+import React, { KeyboardEvent, useState } from "react";
 // components
 import Stage, { StageFormat } from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 // style
 import { TetrisWrapper, TetrisContainer } from "./styles/TetrisStyle";
+// util
+import { createStage } from "../util/gameHelper";
 // hooks
 import { useStage } from "../hooks/useStage";
+import { usePlayer } from "../hooks/usePlayer";
 
 const Tetris = () => {
   const [gameOver] = useState(false);
 
-  const [stage] = useStage();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("Rendering!"); // 리렌더링 확인을 위한 로그
+
+  const startGame = () => {
+    // Reset Everything
+    setStage(createStage() as StageFormat);
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const movePlayer = (dir: number) => {
+    updatePlayerPos({ x: dir, y: 0, collided: false });
+  };
+
+  const move = ({ key }: KeyboardEvent<HTMLDivElement>) => {
+    if (!gameOver) {
+      if (key === "ArrowLeft") {
+        movePlayer(-1);
+      } else if (key === "ArrowRight") {
+        movePlayer(1);
+      } else if (key === "ArrowDown") {
+        dropPlayer();
+      } else if (key === "ArrowUp") {
+        // rotate tetromino
+      }
+    }
+  };
+
   return (
-    <TetrisWrapper>
+    <TetrisWrapper role="button" onKeyDown={(e) => move(e)}>
       <TetrisContainer>
         <Stage stage={stage as StageFormat} />
         <aside>
@@ -28,7 +65,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClickFn={startGame} />
         </aside>
       </TetrisContainer>
     </TetrisWrapper>
--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,17 +1,46 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState, useEffect } from "react";
+// components
+import { StageFormat } from "../components/Stage";
 // util
 import { createStage } from "../util/gameHelper";
-import { StageFormat } from "../components/Stage";
+import { TetrominoType } from "../util/tetrominos";
+// hooks
+import { PlayerState } from "./usePlayer";
 
 /*
 현재 테트리스 게임판의 상태를 저장하기 위한 hook
 */
 
-export const useStage = (): [
-  StageFormat,
-  Dispatch<SetStateAction<StageFormat>>
-] => {
+export const useStage = (
+  player: PlayerState,
+  resetPlayer: () => void
+): [StageFormat, Dispatch<SetStateAction<StageFormat>>] => {
   const [stage, setStage] = useState<StageFormat>(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage: StageFormat): StageFormat => {
+      // 현재 Stage를 그림
+      const newStage: StageFormat = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      // 사용자가 조작하는 테트리스 블록을 그림
+      player.tetromino.forEach((row, y: number) => {
+        row.forEach((value: TetrominoType, x: number) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+
+      return newStage;
+    };
+
+    setStage((prev: StageFormat) => updateStage(prev));
+  }, [player]);
 
   return [stage, setStage];
 };
